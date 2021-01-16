17971a3e6f65b9a3289de94c89fc60069806fde5
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2021-01-16 22:31:28 +0330

    Implement Utils & Structure Of The SimpleTokenContract

--- TokenContractUtils.sol
+++ TokenContractUtils.sol
@@ -15,4 +15,21 @@ contract TokenContractUtils {
         require(_address != address(0));
         _;
     }
+
+    function recover(bytes32 hash, bytes sig) public pure returns (address) {
+        if (sig.length != 65) {return (address(0));}
+
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
+
+        if (v < 27) {v += 27;}
+
+        return (v == 27 || v == 28) ? ecrecover(hash, v, r, s) : address(0);
+    }
 }
