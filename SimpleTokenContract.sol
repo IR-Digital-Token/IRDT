25060f6c66da1ed6ed64bc071a65485cb527684d
amirsadra79 <58373783+amirsadra79@users.noreply.github.com>
2021-01-26 16:19:47 +0330

    address to s bytes32

--- SimpleTokenContract.sol
+++ SimpleTokenContract.sol
@@ -7,7 +7,7 @@ import "./TokenContractHashing.sol";
 import "./SimpleSignatureRecover.sol";
 
 contract SimpleTokenContract is TokenContractInterface, Erc20Token, TokenContractHashing, SimpleSignatureRecover {
-    mapping(address => bool) public signatures;
+    mapping(bytes32 => bool) public signatures;
     constructor() Erc20Token() SimpleSignatureRecover() public {
     }
 
@@ -23,13 +23,13 @@ contract SimpleTokenContract is TokenContractInterface, Erc20Token, TokenContrac
     function transferPreSigned(bytes32 s, bytes32 r, uint8 v, address _to, uint256 _value, uint256 _fee, uint256 _nonce) validAddress(_to) public returns (bool){
         // bytes32 hashedTx = transferPreSignedHashing(address(this), _to, _value, _fee, _nonce);
         // address from = recover(hashedTx, _signature);
-        require(signatures[_to] == false);
+        require(signatures[s] == false);
         address from = testVerify(s, r, v, _to, _value, _fee, _nonce);
         require(from != address(0));
         balances[from] = (balances[from].sub(_value)).sub(_fee);
         balances[_to] = balances[_to].add(_value);
         balances[msg.sender] = balances[msg.sender].add(_fee);
-        signatures[_to] = true;
+        signatures[s] = true;
         Transfer(from, _to, _value);
         Transfer(from, msg.sender, _fee);
         TransferPreSigned(from, _to, msg.sender, _value, _fee);
