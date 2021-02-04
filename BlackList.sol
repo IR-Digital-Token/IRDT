4d144be8a11a38a00f2864459759a51e281ae077
Saber Zafarpoor <45599139+SaberDoTcodeR@users.noreply.github.com>
2021-02-04 12:56:37 +0330

    Create BlackList.sol

---
+++ BlackList.sol
@@ -0,0 +1,33 @@
+contract BlackList {
+
+    mapping (address => bool) public isBlackListed;
+    
+    event DestroyedBlackFunds(address _blackListedUser, uint _balance);
+
+    event AddedBlackList(address _user);
+
+    event RemovedBlackList(address _user);
+    
+    function getBlackListStatus(address addr) public returns (bool) {
+        return isBlackListed[addr];
+    }
+    
+    function addBlackList (address _evilUser) public onlyOwner {
+        isBlackListed[_evilUser] = true;
+        AddedBlackList(_evilUser);
+    }
+
+    function removeBlackList (address _clearedUser) public onlyOwner {
+        isBlackListed[_clearedUser] = false;
+        RemovedBlackList(_clearedUser);
+    }
+
+    function destroyBlackFunds (address _blackListedUser) public onlyOwner {
+        require(isBlackListed[_blackListedUser]);
+        uint256 dirtyFunds = balanceOf(_blackListedUser);
+        balances[_blackListedUser] = 0;
+        _totalSupply = _totalSupply.sub(dirtyFunds);
+        DestroyedBlackFunds(_blackListedUser, dirtyFunds);
+    }
+
+}
